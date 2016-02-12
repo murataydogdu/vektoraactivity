d0936cbe2f2f70d0d9d672535d65e63ff4076d3c
murataydogdu <aydogdumurat@Murats-MacBook-Air.local>
2016-02-12 03:40:39 +0200

    VSD OData api call added
    FIXME : Should be separated api.js file, for now it test purpose
    calling from main js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 var React = require('react-native');
 var styles = require('./styles/mainstyles');
 var Icon = require('react-native-vector-icons/FontAwesome');
+var base64 = require('base-64/base64');
+var Moment = require('moment');
 
 var {
   View,
@@ -15,6 +17,8 @@ var {
 
 //FIXME image source should be local for device (both ios and android)
 var VEKTORALOGO = 'http://www.vektora.com/images/logo.png';
+//FIXME purpose of test - proper way is api.js should seperated -VSDURL
+var VSDURL = "http://vkt-sap-erp01.vektora.com:8010/sap/opu/odata/SAP/Z_VKT_MBL_SRV/";
 
 module.exports = React.createClass({
   getInitialState : function(){
@@ -27,6 +31,26 @@ module.exports = React.createClass({
   //FIXME when keybord open and close handle the view
   scrolldown: function(){
   this.refs.scrollView.scrollTo(200*2/3);
+  },
+  onPressLogin : function(){
+    var headers = new Headers();
+    headers.append("Authorization", "Basic " + base64.encode(this.state.username+":"+this.state.password));
+
+    var param = "DateSet?$filter=WorkDate eq datetime ";
+    var datetime = Moment().format();
+
+    fetch(VSDURL+param+'&$format=json', {
+        headers: headers
+      })
+      .then(function(response){
+        return response.json()
+      })
+      .then(function(json){
+        console.log(json);
+      });
+
+    // console.log(Moment().format());
+    // console.log(this.state.password);
   },
   render : function(){
     return(
@@ -45,6 +69,8 @@ module.exports = React.createClass({
           <TextInput
             style={styles.usernametextinput}
             placeholder={'SAP Kullanıcı adı'}
+            autoCorrect={false}
+            autoCapitalize={'none'}
             onChangeText={(username) => this.setState({username})}
             onFocus={this.scrolldown}
             value={this.state.username} />
